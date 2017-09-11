54fadfea684e1ca1edf981de79ea2f2be940ba99
KELLY\yeseul <yesel6089@gmail.com>
2017-09-11 22:52:46 +0900

    map(str, list) 추가

--- Map.py
+++ Map.py
@@ -1,11 +1,16 @@
 
 # map function define
 
+
 def f(x):
     return x+x
 
 def map(f,sequence):
+
     result = ()
+
+    # map 은 시퀀스의 모든 인수에 인수가 하나인 함수를 적용하고 결괏값을 튜플로 생성
+
     for a in sequence:
         result += (f(a),)
     return result
@@ -13,6 +18,11 @@ def map(f,sequence):
 sequence = ("apple","banana","peach")
 
 print(map(f,sequence))
+
+# 숫자 list (sequence)를 str으로 바꾸어 tuple을 생성하는 map
+print(map(str,[1,2,3]))
+
+print(', '.join(map(str,[4,5,6])))
 
 
 
